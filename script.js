b6b6e4e654518f789ab49fdd450aeeefac516c5e
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-22 14:40:50 +0545

    feat: Add spreadsheet functionality
    - Update spread sheet cells with user input
        - Evaluate spreadsheet formulas
        - Apply spreadsheet functions
        - Determine operator prededence
        - Evaluate and apply infix operators

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// === START OF SPREADSHEET HELPER FUNCTIONS ===
+
+// Map infix operator to relevant function
+const infixToFunction = {
+    "+": (x, y) => x + y,
+    "-": (x, y) => x - y,
+    "*": (x, y) => x * y,
+    "/": (x, y) => x / y,
+};
+
+// Evaluate infix functions
+const infixEval = (str, regex) =>
+    str.replace(regex, (_match, arg1, operator, arg2) =>
+        infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
+    );
+
+// Operator precedence
+const highPrecedence = (str) => {
+    const regex = /([\d.]+)([*\/])([\d.]+)/;
+    const str2 = infixEval(str, regex);
+    return str === str2 ? str : highPrecedence(str2);
+};
+
 // Check if a number is even
 const isEven = (num) => num % 2 === 0;
 
@@ -19,10 +42,38 @@ const median = (nums) => {
 
 // Collection of spreadsheet functions
 const spreadsheetFunctions = {
+    "": (arg) => arg,
     sum,
     average,
     median,
+    even: (nums) => nums.filter(isEven),
+    someeven: (nums) => nums.some(isEven),
+    everyeven: (nums) => nums.every(isEven),
+    firsttwo: (nums) => nums.slice(0, 2),
+    lasttwo: (nums) => nums.slice(-2),
+    has2: (nums) => nums.includes(2),
+    increment: (nums) => nums.map((num) => num + 1),
+    random: ([x, y]) => Math.floor(Math.random() * y + x),
+    range: (nums) => range(...nums),
+    nodupes: (nums) => [...new Set(nums).values()],
 };
+// Apply function by parsing logic to string
+const applyFunction = (str) => {
+    const noHigh = highPrecedence(str);
+    const infix = /([\d.]+)([+-])([\d.]+)/;
+    const str2 = infixEval(noHigh, infix);
+    const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+    const toNumberList = (args) => args.split(",").map(parseFloat);
+    const apply = (fn, args) =>
+        spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+    return str2.replace(functionCall, (match, fn, args) =>
+        spreadsheetFunctions.hasOwnProperty(fn.toLowerCase())
+            ? apply(fn, args)
+            : match
+    );
+};
+
+// === END OF SPREADSHEET HELPER FUNCTIONS ===
 
 // Generate a range of numbers
 const range = (start, end) =>
@@ -35,6 +86,30 @@ const charRange = (start, end) =>
     range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
         String.fromCharCode(code)
     );
+
+// Parse and evaluate string
+const evalFormula = (x, cells) => {
+    const idToText = (id) => cells.find((cell) => cell.id === id).value;
+    const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+    const rangeFromString = (num1, num2) =>
+        range(parseInt(num1), parseInt(num2));
+    const elemValue = (num) => (character) => idToText(character + num);
+    const addCharacters = (character1) => (character2) => (num) =>
+        charRange(character1, character2).map(elemValue(num));
+    const rangeExpanded = x.replace(
+        rangeRegex,
+        (_match, char1, num1, char2, num2) =>
+            rangeFromString(num1, num2).map(addCharacters(char1)(char2))
+    );
+    const cellRegex = /[A-J][1-9][0-9]?/gi;
+    const cellExpanded = rangeExpanded.replace(cellRegex, (match) =>
+        idToText(match.toUpperCase())
+    );
+    const functionExpanded = applyFunction(cellExpanded);
+    return functionExpanded === x
+        ? functionExpanded
+        : evalFormula(functionExpanded, cells);
+};
 
 window.onload = () => {
     const container = document.getElementById("container");
@@ -55,7 +130,23 @@ window.onload = () => {
             input.type = "text";
             input.id = letter + number;
             input.ariaLabel = letter + number;
+            input.onchange = update;
             container.appendChild(input);
         });
     });
 };
+
+// === START OF SPREADSHEET FUNCTIONS ===
+
+// Update
+const update = (event) => {
+    const element = event.target;
+    const value = element.value.replace(/\s/g, "");
+    if (!value.includes(element.id) && value.startsWith("=")) {
+        element.value = evalFormula(
+            value.slice(1),
+            Array.from(document.getElementById("container").children)
+        );
+    }
+};
+// === END OF SPREADSHEET FUNCTIONS ===
