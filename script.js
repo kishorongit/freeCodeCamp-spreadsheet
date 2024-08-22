464df0b026a7d7fb89ce77f195ca611b7add71b6
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-22 10:44:23 +0545

    feat: Add spreadsheet helper functions
    - Check if a number is even
    
    - Calculate sum of a list of numbers
    
    - Calculate average of a list of numbers
    
    - Calculate median of a list of numbers

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// Check if a number is even
+const isEven = (num) => num % 2 === 0;
+
+// Calculate sum of numbers
+const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
+
+// Calculate average
+const average = (nums) => sum(nums) / nums.length;
+
+// Calculate median
+const median = (nums) => {
+    const sorted = nums.slice().sort((a, b) => a - b);
+    const length = sorted.length;
+    const middle = length / 2 - 1;
+    return isEven(length)
+        ? average([sorted[middle], sorted[middle + 1]])
+        : sorted[Math.ceil(middle)];
+};
+
+// Collection of spreadsheet functions
+const spreadsheetFunctions = {
+    sum,
+    average,
+    median,
+};
+
 // Generate a range of numbers
 const range = (start, end) =>
     Array(end - start + 1)
